9eb61014d4371f0715a2e5e98bcdba2d7851d590
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 14:34:37 -0500

    storage

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Storage} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+async function putFile() {
+  const result = await Storage.put('test.txt', 'Hello');
+  console.log(result);
+}
+putFile();
+
+Storage.list('') // for listing ALL files without prefix, pass '' instead
+  .then(({results}) => console.log(results))
+  .catch(err => console.log(err));
 
 export default function App() {
   return (
