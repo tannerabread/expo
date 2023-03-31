fb917cff03005562bdd9333a23a47ddf333018c9
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-31 08:02:02 -0500

    rest api with auth calls

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, API, Auth} from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+Amplify.configure({
+  ...config,
+  API: {
+    endpoints: [
+      {
+        name: 'apiauth',
+        endpoint: config.aws_cloud_logic_custom[1].endpoint,
+        region: config.aws_cloud_logic_custom[1].region,
+        custom_header: async () => {
+          return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+        }
+      }
+    ]
+  }
+});
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+API.post('apiauth', '/items', {body: {name: 'item1'}})
+  .then(response => {
+    console.log({response});
+  })
+  .catch(error => {
+    console.log(error.response);
+  });
+
+API.get('apiauth', '/items', {})
+  .then(response => {
+    console.log({response});
+  })
+  .catch(error => {
+    console.log(error.response);
+  });
+
 
 export default function App() {
   return (
