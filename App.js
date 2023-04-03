29fa3ed48c8d6f0300c5e88f007aad414eb06fdb
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-04-03 12:21:33 -0500

    datastore updates

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-import {Amplify} from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
-Amplify.Logger.LOG_LEVEL = 'DEBUG';
+import { Amplify, DataStore } from "aws-amplify";
+import { Todo } from "./models";
+import { SQLiteAdapter } from '@aws-amplify/datastore-storage-adapter/SQLiteAdapter';
+import 'core-js/full/symbol/async-iterator';
+import config from "./aws-exports";
+Amplify.configure({
+  ...config,
+  DataStore: {
+    storageAdapter: SQLiteAdapter,
+    authModeStrategyType: "MULTI_AUTH",
+  }
+});
+Amplify.Logger.LOG_LEVEL = "DEBUG";
+
+async function saveDatastoreTodo() {
+  try {
+    const post = await DataStore.save(
+      new Todo({
+        name: "Datastore",
+        description: "hello datastore!",
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+saveDatastoreTodo();
+
+async function readDatastore() {
+  try {
+    const todos = await DataStore.query(Todo);
+    console.log("todos: ", JSON.stringify(todos, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+}
+readDatastore();
 
 export default function App() {
   return (
@@ -18,8 +51,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
